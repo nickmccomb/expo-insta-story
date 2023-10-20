40c43def7c1fede0967d49ef47e067eb304bc010
Nicholas McComb <nickmccomb1984@gmail.com>
2023-10-20 23:31:09 +1100

    save

--- src/StoryListItem.tsx
+++ src/StoryListItem.tsx
@@ -442,10 +442,7 @@ const styles = StyleSheet.create({
     marginTop: 5,
   },
   video: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    bottom: 0,
-    right: 0,
+    width: '100%',
+    height: '100%',
   },
 });
